73f5047ec76e8ad6d6f3f89fe78c0766c388d462
miguelafsetec <miguel.silva236@etec.sp.gov.br>
2024-09-23 17:32:07 -0300

    miguel

---
+++ projeto/projeto/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace projeto
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string CPF;
+        public string Endereço;
+        public int idade;
+        public int Ano_de_Nascimento;
+        public int dia;
+        public int mes;
+        public string email;
+        public string prioridade;
+        public string telefone;
+
+
+
+        public virtual void  cadastro()
+        {
+            Console.WriteLine("Digite seu nome Completo");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite seu cpf");
+            CPF = Console.ReadLine();
+            Console.WriteLine("digite seu Endereço");
+            Endereço = Console.ReadLine();
+            Console.WriteLine("Qual sua idade");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o ano do seu ");
+            idade = int.Parse(Console.ReadLine());
+
+
+
+        }
+
+
+
+    }
+}
---
+++ projeto/projeto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] pessoa = new Pessoa[100];
+            
+            for(int i = 0; i < 2; i++)
+            {
+
+                Console.WriteLine("Pessoa {0}",i);
+                pessoa[i] = new Pessoa();
+                pessoa[i].cadastro();
+                pessoa[i].prioridade=Console.ReadLine();
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                if (pessoa[i].prioridade == "vermelho")
+                {
+                    Console.WriteLine($"nome: {pessoa[i].nome}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
